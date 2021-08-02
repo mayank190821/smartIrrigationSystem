92cd9f98740f17677bf2dd956e2a6c0e23644a6d
rupin-1801 <rupinbhugra1411@gmail.com>
2021-08-02 23:13:27 +0530

    completed main activity implementation

--- app_source_code/java/com/logic_api/smartirrigation/MainActivity.java
+++ app_source_code/java/com/logic_api/smartirrigation/MainActivity.java
@@ -18,11 +18,72 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity{
 
+    int moisture = 0, temperature=0, humidity=0;
+    String longitude="50.77°E", latitude="340.56°N";
+    ProgressBar moistureProgress, humidityProgress, temperatureProgress;
+    TextView moistureValue, temperatureValue, humidityValue, vLongitude, vLatitude;
+    ImageButton imageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        moistureProgress = findViewById(R.id.progress1);
+        humidityProgress = findViewById(R.id.progress2);
+        temperatureProgress = findViewById(R.id.progress3);
+        vLongitude = findViewById(R.id.longitude);
+        vLatitude = findViewById(R.id.latitude);
+        moistureValue = findViewById(R.id.value1);
+        humidityValue = findViewById(R.id.value2);
+        temperatureValue = findViewById(R.id.value3);
+        imageButton = findViewById(R.id.imageButton);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                moisture = Integer.parseInt(Objects.requireNonNull(snapshot.child("moisture").getValue(String.class)));
+                temperature = Integer.parseInt(Objects.requireNonNull(snapshot.child("temperature").getValue(String.class)));
+                humidity = Integer.parseInt(Objects.requireNonNull(snapshot.child("humidity").getValue(String.class)));
+                longitude = snapshot.child("longitude").getValue(String.class);
+                latitude = snapshot.child("latitude").getValue(String.class);
+                updateData();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        updateData();
+
+        imageButton.setOnClickListener(view -> {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), AuthenticationActivity.class));
+                finish();
+        });
     }
 
+    private void updateData() {
+        moistureProgress.setProgress(moisture);
+        String textValue = moisture + "%";
+        moistureValue.setText(textValue);
+
+        temperatureProgress.setProgress(temperature);
+        textValue = temperature + "°C";
+        temperatureValue.setText(textValue);
+
+        humidityProgress.setProgress(humidity);
+        textValue = humidity + "%";
+        humidityValue.setText(textValue);
+
+        textValue = "Longitude : " + longitude;
+        vLongitude.setText(textValue);
+        textValue = "Latitude : " + latitude;
+        vLatitude.setText(textValue);
+    }
 }
