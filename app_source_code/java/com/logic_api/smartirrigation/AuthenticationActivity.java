0712d19c04a551d2d2d1f9feb663dd69087acf02
rupin-1801 <rupinbhugra1411@gmail.com>
2021-08-02 23:08:56 +0530

    completed Authentication via otp

--- app_source_code/java/com/logic_api/smartirrigation/AuthenticationActivity.java
+++ app_source_code/java/com/logic_api/smartirrigation/AuthenticationActivity.java
@@ -14,13 +14,80 @@ import java.util.concurrent.TimeUnit;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+
+    String verificationCodeBySystem;
+    Button nextButton, sendButton;
+    EditText phone;
+    EditText otp;
+
+
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        nextButton = findViewById(R.id.nextButton);
+        phone = findViewById(R.id.phone);
+        otp = findViewById(R.id.otp);
+        sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(view -> sendVerificationCodeToUser(phone.getText().toString()));
+        nextButton.setOnClickListener(view -> verifyCode(otp.getText().toString()));
     }
 
-    
+    private void sendVerificationCodeToUser(String phoneNo) {
+        String number = "+91"+phoneNo;
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                AuthenticationActivity.this,
+                mCallbacks
+        );
+    }
+
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationCodeBySystem = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if(code != null) {
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(AuthenticationActivity.this, "failed", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCodeBySystem, code);
+        signInUserByCredentials(credential);
+    }
+
+    private void signInUserByCredentials(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(AuthenticationActivity.this, task -> {
+                    if(task.isSuccessful()){
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+
+                    }
+                    else{
+                        Toast.makeText(AuthenticationActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
 
 
