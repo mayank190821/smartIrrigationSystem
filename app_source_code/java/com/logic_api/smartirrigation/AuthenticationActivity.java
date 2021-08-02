dbd541655c5301e5ca2e30a78f568d7fa9d30669
rupin-1801 <rupinbhugra1411@gmail.com>
2021-08-02 23:07:54 +0530

    added Authentication activity

---
+++ app_source_code/java/com/logic_api/smartirrigation/AuthenticationActivity.java
@@ -0,0 +1,27 @@
+package com.logic_api.smartirrigation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.*;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+
+    }
+
+    
+}
+
+
+
